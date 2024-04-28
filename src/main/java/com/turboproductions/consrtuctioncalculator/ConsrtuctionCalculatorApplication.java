68c10b52d4a4aa60002b0730961e46fe300264a5
sa6iko1501 <aleksandstoianov@tu-sofia.bg>
2024-04-28 21:52:56 +0300

    CC-1

---
+++ src/main/java/com/turboproductions/consrtuctioncalculator/ConsrtuctionCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.turboproductions.consrtuctioncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsrtuctionCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsrtuctionCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turboproductions/consrtuctioncalculator/ConsrtuctionCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turboproductions.consrtuctioncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsrtuctionCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
