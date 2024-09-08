af898083a88c2394e8545a8a144ecfa8ab1c8d61
sa6iko1501 <aleksandstoianov@tu-sofia.bg>
2024-09-08 18:05:34 +0300

    CC-19: formatting

--- src/main/java/com/turboproductions/consrtuctioncalculator/dao/ConstructionCalculationRepository.java
+++ src/main/java/com/turboproductions/consrtuctioncalculator/dao/ConstructionCalculationRepository.java
@@ -10,5 +10,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ConstructionCalculationRepository
     extends JpaRepository<ConstructionCalculation, UUID> {
   List<ConstructionCalculation> findConstructionCalculationsByUserOrderByDate(User user);
-
 }
